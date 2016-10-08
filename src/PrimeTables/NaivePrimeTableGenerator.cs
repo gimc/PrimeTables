b81c02b6339ea58f5656535a96a0d0e04e79df4c
Gordon McAllister <gordonmc8@gmail.com>
2016-10-08 12:50:41 +0100

    Naive implementation of table generation

---
+++ src/PrimeTables/NaivePrimeTableGenerator.cs
@@ -0,0 +1,42 @@
+﻿namespace PrimeTables
+{
+    /// <summary>
+    /// Uses a naive implementation whereby the result is recalculated for every entry in the table.
+    /// </summary>
+    public class NaivePrimeTableGenerator : IPrimeTableGenerator
+    {
+        private readonly IPrimeSequenceGenerator _primeSequenceGenerator;
+
+        public NaivePrimeTableGenerator(IPrimeSequenceGenerator primeSequenceGenerator)
+        {
+            _primeSequenceGenerator = primeSequenceGenerator;
+        }
+
+        public int[,] Generate(int numPrimes)
+        {
+            var table = new int[numPrimes, numPrimes];
+            var primeList = GeneratePrimeList(numPrimes);
+
+            for (var row = 0; row < numPrimes; row++)
+            {
+                for (var col = 0; col < numPrimes; col++)
+                {
+                    table[row, col] = primeList[row]*primeList[col];
+                }
+            }
+
+            return table;
+        }
+
+        private int[] GeneratePrimeList(int numPrimes)
+        {
+            var primeList = new int[numPrimes];
+            for (var i = 0; i < numPrimes; i++)
+            {
+                primeList[i] = _primeSequenceGenerator.Next();
+            }
+
+            return primeList;
+        }
+    }
+}
--- tests/PrimeTables.Tests/PrimeTableGeneratorTests.cs
+++ tests/PrimeTables.Tests/PrimeTableGeneratorTests.cs
@@ -5,7 +5,7 @@ namespace PrimeTables.Tests
     class PrimeTableGeneratorTests
     {
         [Test]
-        public void ThreeByThreeTable()
+        public void NaiveThreeByThreeTable()
         {
             var expected = new [,]
             {
@@ -14,8 +14,26 @@ namespace PrimeTables.Tests
                 {10, 15, 25}
             };
 
-            var generator = new PrimeTableGenerator(new PrimeSequenceGenerator());
+            var generator = new NaivePrimeTableGenerator(new PrimeSequenceGenerator());
             var table = generator.Generate(3);
+
+            Assert.AreEqual(expected, table);
+        }
+
+        [Test]
+        public void NaiveFiveByFiveTable()
+        {
+            var expected = new[,]
+            {
+                {4, 6, 10, 14, 22},
+                {6, 9, 15, 21, 33},
+                {10, 15, 25, 35, 55},
+                {14, 21, 35, 49, 77},
+                {22, 33, 55, 77, 121}
+            };
+
+            var generator = new NaivePrimeTableGenerator(new PrimeSequenceGenerator());
+            var table = generator.Generate(5);
 
             Assert.AreEqual(expected, table);
         }
