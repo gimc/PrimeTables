8345eaf53eb09cca2bfaede0c3bd596f46383c29
Gordon McAllister <gordonmc8@gmail.com>
2016-10-08 12:21:26 +0100

    Initial implementation of generator logic + tests

--- src/PrimeTables/PrimeSequenceGenerator.cs
+++ src/PrimeTables/PrimeSequenceGenerator.cs
@@ -1,13 +1,50 @@
-﻿namespace PrimeTables
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeTables
 {
     /// <summary>
     /// Generates a sequence of primes, starting at 2
     /// </summary>
     public class PrimeSequenceGenerator : IPrimeSequenceGenerator
     {
+        private List<int> _primes = new List<int>();
+        private int _current;
+
+        public PrimeSequenceGenerator()
+        {
+            _current = 1;
+        }
+
+
         public int Next()
         {
-            return 2;
+            FindNextPrime();
+            return _current;
+        }
+
+        private void FindNextPrime()
+        {
+            var candidate = _current + 1;
+
+            while (true)
+            {
+                // If candidate is even, can't be prime except 2 which is a special even case
+                if (candidate%2 == 0 && candidate != 2)
+                {
+                    candidate += 1;
+                }
+
+                if (_primes.Any(prime => candidate%prime == 0))
+                {
+                    candidate++;
+                    continue;
+                }
+
+                _current = candidate;
+                _primes.Add(candidate);
+                return;
+            }
         }
     }
 }
--- tests/PrimeTables.Tests/PrimeSequenceGeneratorTests.cs
+++ tests/PrimeTables.Tests/PrimeSequenceGeneratorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace PrimeTables.Tests
 {
@@ -11,6 +12,34 @@ namespace PrimeTables.Tests
             var prime = generator.Next();
 
             Assert.AreEqual(2, prime);
+        }
+
+        [Test]
+        public void GenerateSequence_HundredthPrime()
+        {
+            const int hundredthPrime = 541;
+            var generator = new PrimeSequenceGenerator();
+            var prime = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                prime = generator.Next();
+            }
+
+            Assert.AreEqual(hundredthPrime, prime);
+        }
+
+        [Test]
+        public void GenerateSequence_FirstFivePrimes()
+        {
+            var expected = new[] {2, 3, 5, 7, 11};
+            var generator = new PrimeSequenceGenerator();
+            var generated = new List<int>();
+            for (var i = 0; i < 5; i++)
+            {
+                generated.Add(generator.Next());
+            }
+
+            Assert.AreEqual(expected, generated.ToArray());
         }
     }
 }
