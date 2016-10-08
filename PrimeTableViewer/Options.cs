b65d806893cf6105ff0d683bb0ed647a4c9e5aff
Gordon McAllister <gordonmc8@gmail.com>
2016-10-08 14:22:17 +0100

    Added project for console application to display prime tables

---
+++ PrimeTableViewer/Options.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace PrimeTableViewer
+{
+    public class Options : IParserState
+    {
+        [Option('n', "NumPrimes", HelpText = "The number of primes in the generated table.", DefaultValue = 2)]
+        public int NumPrimes { get; set; }
+
+        [ParserState]
+        public ParserState LastParserState { get; set; }
+
+        public IList<ParsingError> Errors { get; private set; }
+    }
+}
---
+++ PrimeTableViewer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+using PrimeTables;
+
+namespace PrimeTableViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options))
+            {
+                Console.WriteLine(HelpText.AutoBuild(options));
+                return;
+            }
+
+            var sequenceGenerator = new PrimeSequenceGenerator();
+            var tableGenerator = new NaivePrimeTableGenerator(sequenceGenerator);
+            var view = new PrimeTableTextView(tableGenerator);
+
+            try
+            {
+                var output = view.Generate(options.NumPrimes);
+                Console.Write(output);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Could not generate prime tables for " + options.NumPrimes + " primes (number of primes must be greater than zero).");
+            }
+        }
+    }
+}
