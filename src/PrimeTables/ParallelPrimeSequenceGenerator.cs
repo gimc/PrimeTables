f99ceac6e3cf9b1cceb7e4717e755dfd936a447b
Gordon McAllister <gordonmc8@gmail.com>
2016-10-09 21:47:58 +0100

    Added implementation of IPrimeSequenceGenerator which uses Task parallelism in an attempt to speed up computation.

---
+++ src/PrimeTables/ParallelPrimeSequenceGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeTables
+{
+    /// <summary>
+    /// Spawns a number of tasks (defined by numTasks) in parallel which each interrogate their own non-overlapping subsets of primes to determine primality of a candidate value.
+    /// </summary>
+    public class ParallelPrimeSequenceGenerator : IPrimeSequenceGenerator
+    {
+        private readonly int _numTasks;
+        private readonly List<long>[] _primeLists;
+        private int _currentValue;
+        private int _currentPrimeList;
+
+        public ParallelPrimeSequenceGenerator(int numTasks)
+        {
+            _numTasks = numTasks;
+
+            _primeLists = new List<long>[_numTasks];
+            for (var i = 0; i < _numTasks; i++)
+            {
+                _primeLists[i] = new List<long>();
+            }
+
+            _currentValue = 1;
+            _currentPrimeList = 0;
+        }
+
+        public int Next()
+        {
+            FindNextPrime();
+            return _currentValue;
+        }
+
+        private bool IsPrime(int candidate)
+        {
+            var tasks = new Task<bool>[_numTasks];
+            for (var i = 0; i < _numTasks; i++)
+            {
+                var closureIndex = i;
+                tasks[i] = Task.Run(() => _primeLists[closureIndex].All(prime => candidate % prime != 0));
+            }
+
+            Task.WaitAll(tasks);
+
+            return tasks.All(task => task.Result);            
+        }
+
+        private void FindNextPrime()
+        {
+            var candidate = _currentValue + 1;
+            while (!IsPrime(candidate))
+            {
+                candidate++;
+            }
+
+            _currentValue = candidate;
+            _primeLists[_currentPrimeList].Add(candidate);
+            _currentPrimeList = (_currentPrimeList + 1) % _numTasks;
+        }
+    }
+}
--- tests/PrimeTables.Tests/PrimeSequenceGeneratorTests.cs
+++ tests/PrimeTables.Tests/PrimeSequenceGeneratorTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using NUnit.Framework;
 
 namespace PrimeTables.Tests
@@ -29,6 +31,20 @@ namespace PrimeTables.Tests
         }
 
         [Test]
+        public void GenerateSequenceParallel_HundredthPrime()
+        {
+            const int hundredthPrime = 541;
+            var generator = new ParallelPrimeSequenceGenerator(4);
+            var prime = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                prime = generator.Next();
+            }
+
+            Assert.AreEqual(hundredthPrime, prime);
+        }
+        
+        [Test]
         public void GenerateSequence_FirstFivePrimes()
         {
             var expected = new[] {2, 3, 5, 7, 11};
@@ -43,15 +59,40 @@ namespace PrimeTables.Tests
         }
 
         [Test]
-        [Ignore("Long-running, need a better optimised algorithm")]
+        [Ignore("Long running - maybe a million primes is a bit on the optimistic side")]
         public void GenerateSequence_MillionthPrime()
         {
             const int millionthPrime = 179424673;
             var generator = new PrimeSequenceGenerator();
             var prime = 0;
+            var sw = new Stopwatch();
+            sw.Start();
             for (var i = 0; i < 1000000; i++)
             {
                 prime = generator.Next();
+                
+                if (i % 10000 == 0)
+                    Console.WriteLine("Prime " + i + ": " + sw.Elapsed);
+            }
+
+            Assert.AreEqual(millionthPrime, prime);
+        }
+
+        [Test]
+        [Ignore("Long running - maybe a million primes is a bit on the optimistic side")]
+        public void GenerateSequenceParallel_MillionthPrime()
+        {
+            const int millionthPrime = 179424673;
+            var generator = new ParallelPrimeSequenceGenerator(2);
+            var prime = 0;
+            var sw = new Stopwatch();
+            sw.Start();
+            for (var i = 0; i < 1000000; i++)
+            {
+                prime = generator.Next();
+
+                if (i % 10000 == 0)
+                    Console.WriteLine("Prime " + i + ": " + sw.Elapsed);
             }
 
             Assert.AreEqual(millionthPrime, prime);
